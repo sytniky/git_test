bdb9abaa179e27fc49911da41dd1aff717ab7432
ysytnik <sytniky@gmail.com>
2016-03-17 19:52:22 +0300

    Added a new cycle

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,10 @@
  * Created by yuriy on 17.03.16.
  */
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("Git commit");
+        for (int i = 1; i < 10; i++) {
+            System.out.println(i + " - " + i);
+        }
+    }
 }
